3927a5869e161f7cdef0c24bdec99e11e7b24375
diduladishan <diduladishan14@gmail.com>
2025-03-18 16:04:54 +0530

    chore: refactor code for improved readability and optimize responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import AboutUs from "@/components/AboutUs";
-import Camp from "@/components/Camp";
-import Carousel from "@/components/Carousel";
-import Carousel2 from "@/components/Carousel2";
-import Features from "@/components/Features";
-import GetApp from "@/components/GetApp";
-import Guide from "@/components/Guide";
-import Hero from "@/components/Hero";
+
 import HeroSection from "@/components/HeroSection";
 import Marquee from "@/components/Marquee";
 import Reservation from "@/components/Reservation";
